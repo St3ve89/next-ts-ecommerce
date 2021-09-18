ebd735d3f0b98d6b526358f1870b89935dcfb210
St3ve89 <istvan.acs89@gmail.com>
2021-09-18 18:35:24 +0100

    Add infer get static prps type

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div>hello</div>;
+import { InferGetStaticPropsType } from 'next';
+
+export default function Home({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <div>{products}</div>;
+}
+
+export async function getStaticProps() {
+  const products = [1, 2, 3];
+  return {
+    props: {
+      products,
+    },
+    revalidate: 4 * 60 * 60,
+  };
 }
